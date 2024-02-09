1f890683d993266d18734201b70ff4599d5ca2ce
Daniel Mantei <dan.mantei@outlook.com>
2024-02-09 15:07:14 -0800

    base path testing

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ pub fn App() -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context();
 
-    let base_path = "https://diversable.github.io/deployment-gh-pages-csr/";
+    let base_path = "/deployment-gh-pages-csr/";
 
     view! {
 
