ea12cb8791fe77595a1178a2faea40a4d7f86848
Daniel Mantei <dan.mantei@outlook.com>
2024-02-09 13:38:24 -0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use leptos::*;
+use deployment_gh_pages_csr::App;
+
+fn main() {
+    // set up logging
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
+
+    mount_to_body(|| {
+        view! {
+            <App />
+        }
+    })
+}
